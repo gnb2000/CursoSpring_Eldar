075de8d49840a88ca74ed8086c6ead59f77c9456
gnb2000 <barirodriguez@gmail.com>
2022-01-21 10:05:21 -0300

    Control clientes

---
+++ ControlClientes/src/test/java/com/controlClientes/ControlClientes/ControlClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controlClientes.ControlClientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
